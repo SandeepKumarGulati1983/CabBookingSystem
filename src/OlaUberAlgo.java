07f6053d3ed65477155ce13994c6c83a90efe54f
sandeep Kumar <sandeep.g1983@gmail.com>
2018-07-12 11:07:15 +0530

    distance method

--- src/OlaUberAlgo.java
+++ src/OlaUberAlgo.java
@@ -55,4 +55,40 @@ public class OlaUberAlgo {
 		
 	}
 	
+	/*
+	 * this is for finding the distance between car and user 
+	 * https://www.movable-type.co.uk/scripts/latlong.html
+	 * 
+	 * This uses the ‘haversine’ formula to calculate the great-circle distance between two points 
+	 * – that is, the shortest distance over the earth’s surface – 
+	 * giving an ‘as-the-crow-flies’ distance between the points 
+	 * (ignoring any hills they fly over, of course!).
+
+		Haversine formula:	a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+		c = 2 ⋅ atan2( √a, √(1−a) )
+		d = R ⋅ c
+		where	φ is latitude, λ is longitude, R is earth’s radius (mean radius = 6,371km);
+		note that angles need to be in radians to pass to trig functions!
+	 */
+	
+	private double distance(double lat1, double lng1, double lat2, double lng2) {
+
+	    double earthRadius = 6371 ; //3958.75; // in miles, change to 6371 for kilometer output
+
+	    double dLat = Math.toRadians(lat2-lat1);
+	    double dLng = Math.toRadians(lng2-lng1);
+
+	    double sindLat = Math.sin(dLat / 2);
+	    double sindLng = Math.sin(dLng / 2);
+
+	    double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+	        * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
+
+	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+	    double dist = earthRadius * c;
+
+	    return dist; // output distance, in MILES
+	}
+	
 }
